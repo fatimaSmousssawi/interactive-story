bebd0ea8178568bafe7d50ef41e09b1bda83803b
fatimaSmoussawi <fatimasamoussawi@gmail.com>
2020-10-09 18:09:44 +0300

    interactive-story

---
+++ JS/myScript.js
@@ -0,0 +1,118 @@
+
+
+function trust() {
+    var x = document.getElementById("trust");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function bluff() {
+    var x = document.getElementById("bluff");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function lastChance() {
+    var x = document.getElementById("lastChance");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function useGun() {
+    var x = document.getElementById("useGun");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function sacrificeSelf() {
+    var x = document.getElementById("sacrificeSelf");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function execute() {
+    var x = document.getElementById("execute");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function intimidate() {
+    var x = document.getElementById("intimidate");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function moveCloser() {
+    var x = document.getElementById("moveCloser");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function giveUp() {
+    var x = document.getElementById("giveUp");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function refuse() {
+    var x = document.getElementById("refuse");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function useGun2() {
+    var x = document.getElementById("useGun2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function execute2() {
+    var x = document.getElementById("execute2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function intimidate2() {
+    var x = document.getElementById("intimidate2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# interactive-story
+# interactive-story# interactive-story
+We wrote a story similar to a videogame, we took the images from video on youtube and some from google images.
+We built a flowchart on a website specialized in flowchart and graphs building.
+Our website is based on a specific part of the story, the whole story is written in "The Negotiation" document.
+The website is coposed of HTML,CSS and only one repeated function in JavaScript (just for button toggeling).
+The whole exercise was divided to multiple tasks"
+* Coding
+* Story writing
+* FlowChart drawing:
+https://drive.google.com/file/d/1RyuEcVzpJlD3SS7QTr4Iv8txb-lUeWkG/view
+* Website layout pre-designing (in a text document).
+https://docs.google.com/document/d/1jpYxsb420RJY-WdoP0KiprWgaBKg2F171xDTCydyeJo/edit?usp=sharing
+Everyone of us took a task, completed each task after the other and we finished at last before due time.
